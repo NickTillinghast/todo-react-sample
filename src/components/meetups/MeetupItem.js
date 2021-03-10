642f5d6f73029108d8e4e3f10a6f1745a51afeea
Nick Tillinghast <nickt2@cox.net>
2021-03-09 22:27:34 -0700

    connected firebase for meetups and set state in all meetups page

--- src/components/meetups/MeetupItem.js
+++ src/components/meetups/MeetupItem.js
@@ -11,7 +11,7 @@ function MeetupItem(props) {
                 </div>
                 <div className={classes.content}>
                     <h3>{props.title}</h3>
-                    <adress>{props.address}</adress>
+                    <address>{props.address}</address>
                     <p>{props.description}</p>
                 </div>
                 <div className={classes.actions}>
--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,31 +1,63 @@
+import { useState, useEffect } from 'react';
+
+
 import MeetupList from '../components/meetups/MeetupList';
 
-const DUMMY_DATA = [
-    {
-        id: 'm1',
-        title: 'This is a first meetup',
-        image:
-            'https://images.unsplash.com/photo-1487174244970-cd18784bb4a4?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1052&q=80',
-        address: 'Meetupstreet 5, 12345 Meetup City',
-        description:
-            'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
-    },
-    {
-        id: 'm2',
-        title: 'This is a second meetup',
-        image:
-            'https://images.unsplash.com/photo-1487174244970-cd18784bb4a4?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1052&q=80',
-        address: 'Meetupstreet 5, 12345 Meetup City',
-        description:
-            'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
-    },
-];
+// const DUMMY_DATA = [
+//     {
+//         id: 'm1',
+//         title: 'This is a first meetup',
+//         image:
+//             'https://images.unsplash.com/photo-1487174244970-cd18784bb4a4?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1052&q=80',
+//         address: 'Meetupstreet 5, 12345 Meetup City',
+//         description:
+//             'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
+//     },
+//     {
+//         id: 'm2',
+//         title: 'This is a second meetup',
+//         image:
+//             'https://images.unsplash.com/photo-1487174244970-cd18784bb4a4?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1052&q=80',
+//         address: 'Meetupstreet 5, 12345 Meetup City',
+//         description:
+//             'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
+//     },
+// ];
 
 function AllMeetupsPage() {
+    const [isLoading, setIsLoading] = useState(true);
+    const [loadedMeetups, setLoadedMeetups] = useState([]);
+
+    useEffect(() => {
+        setIsLoading(true);
+        fetch(
+            'https://react-next-js-default-rtdb.firebaseio.com/meetups.json'
+        ).then(response => {
+            return response.json()
+        }).then(data => {
+            const meetups = [];
+            for (const key in data) {
+                const meetup = {
+                    id: key,
+                    ...data[key]
+                };
+                meetups.push(meetup)
+            }
+            setIsLoading(false);
+
+            setLoadedMeetups(meetups);
+        });
+    }, []);
+
+    if (isLoading) {
+        return <section>
+            <p>Loading...</p>
+        </section>
+    }
     return (
         <section>
             <h1>All Meetups</h1>
-            <MeetupList meetups={DUMMY_DATA} />
+            <MeetupList meetups={loadedMeetups} />
         </section>
     )
 }
