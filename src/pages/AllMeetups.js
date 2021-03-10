8ef0be834ebd52966054a3bfaf64d97803b2a555
Nick Tillinghast <nickt2@cox.net>
2021-03-09 22:29:53 -0700

    connected firebase for meetups and set state in all meetups page

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -3,27 +3,6 @@ import { useState, useEffect } from 'react';
 
 import MeetupList from '../components/meetups/MeetupList';
 
-// const DUMMY_DATA = [
-//     {
-//         id: 'm1',
-//         title: 'This is a first meetup',
-//         image:
-//             'https://images.unsplash.com/photo-1487174244970-cd18784bb4a4?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1052&q=80',
-//         address: 'Meetupstreet 5, 12345 Meetup City',
-//         description:
-//             'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
-//     },
-//     {
-//         id: 'm2',
-//         title: 'This is a second meetup',
-//         image:
-//             'https://images.unsplash.com/photo-1487174244970-cd18784bb4a4?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1052&q=80',
-//         address: 'Meetupstreet 5, 12345 Meetup City',
-//         description:
-//             'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
-//     },
-// ];
-
 function AllMeetupsPage() {
     const [isLoading, setIsLoading] = useState(true);
     const [loadedMeetups, setLoadedMeetups] = useState([]);
